3ee1786acb4a71666f1b565575b80c58b52108c4
michaelalabi1 <michael.alabi-duobu@amalitech.com>
2022-02-13 23:33:03 +0000

    script to clone the repository into the home directory of the mob_app_usr

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#change directory to home directory of the mob_app_usr user
+cd /home/mob_app_usr/
+
+#clone the repository
+git clone https://github.com/President-Banda/Mobalysis.git
